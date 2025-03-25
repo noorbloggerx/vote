376ac12f1e7f5225072681e2b7d74f72ee3599b2
noorbloggerx <wikihowhukup@gmail.com>
2025-03-25 03:49:05 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
-git clone https://github.com/noorbloggerx/vote.git
-cd vote
-echo "console.log('Hello World!');" > index.js
-git add .
-git commit -m "Added index.js"
-git push origin main
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+let votes = { islam: 0, hindu: 0, christian: 0, atheist: 0, others: 0 };
+
+app.post('/vote', (req, res) => {
+    const { religion } = req.body;
+    if (votes[religion] !== undefined) {
+        votes[religion]++;
+        res.json({ success: true, votes });
+    } else {
+        res.status(400).json({ success: false, message: 'Invalid religion' });
+    }
+});
+
+app.get('/votes', (req, res) => {
+    res.json(votes);
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
